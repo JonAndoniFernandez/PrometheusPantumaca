c0133aab0cecca37ba0a03416602e6dd294c9cd5
JonanFdez <al315182@uji.es>
2020-02-01 17:29:01 +0100

    MainMenu Boceto

---
+++ PrometheusPantumaca/Assets/Resources/Scripts/Menu/BotonDeMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BotonDeMenu : MonoBehaviour
+{
+    public int numBotonUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void BotonSeleccionado(int numboton)
+    {
+        MenuManager manager = FindObjectOfType<MenuManager>();
+        numBotonUI = numboton;
+
+        switch(numBotonUI)
+        {
+            case 0:
+                Application.LoadLevel("Mario");
+                break;
+            case 1:
+                Application.LoadLevel("SampleScene");
+                break;
+            case 2:
+                Application.LoadLevel("MainMenu");
+                break;
+        }
+
+    }
+}
---
+++ PrometheusPantumaca/Assets/Resources/Scripts/Menu/MenuManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject[] opcionesMenu;
+    [SerializeField] private Transform puntoSeleccionador;
+    [SerializeField] int opcionSeleccionada = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        puntoSeleccionador.position = new Vector3(opcionesMenu[0].transform.position.x - 150, opcionesMenu[0].transform.position.y, opcionesMenu[0].transform.position.z) ;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int auxY = (int) Input.GetAxis("Vertical");
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            CambiarOpcion(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.S))
+        {
+            CambiarOpcion(1);
+        }
+        else if (Input.GetKeyDown(KeyCode.Space))
+        {
+            SeleccionarOpcion(opcionSeleccionada);
+        }
+
+            ;
+    }
+
+    void CambiarOpcion(int cambio)
+    {
+        opcionSeleccionada += cambio ;
+        if (opcionSeleccionada > 2)
+            opcionSeleccionada = 0;
+        else if (opcionSeleccionada< 0)
+        {
+            opcionSeleccionada = 2; 
+        }
+
+        if (opcionSeleccionada == 0)
+        {
+            puntoSeleccionador.position = new Vector3(opcionesMenu[0].transform.position.x - 150, opcionesMenu[0].transform.position.y, opcionesMenu[0].transform.position.z);
+        } else if(opcionSeleccionada == 1)
+        {
+            puntoSeleccionador.position = new Vector3(opcionesMenu[1].transform.position.x - 150, opcionesMenu[1].transform.position.y, opcionesMenu[1].transform.position.z);
+        }
+        else if(opcionSeleccionada == 2){
+            puntoSeleccionador.position = new Vector3(opcionesMenu[2].transform.position.x - 150, opcionesMenu[2].transform.position.y, opcionesMenu[2].transform.position.z);
+        }
+
+        
+    }
+
+    void SeleccionarOpcion(int seleccion)
+    {
+        opcionesMenu[0].GetComponent<BotonDeMenu>().BotonSeleccionado(opcionSeleccionada);
+    }
+}
