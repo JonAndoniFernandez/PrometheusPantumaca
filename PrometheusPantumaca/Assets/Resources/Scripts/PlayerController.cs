f2dc476fdd7c3a46837fc6f518c49c01d4e970ab
JonanFdez <al315182@uji.es>
2020-02-01 02:02:35 +0100

    Maxi push inservible

---
+++ PrometheusPantumaca/Assets/Resources/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    
+
+    public float axisY, axisX;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ControlarMovimiento();
+    }
+
+    private void ControlarMovimiento()
+    {
+        axisY = Input.GetAxis("Vertical");
+        axisX = Input.GetAxis("Horizontal");
+
+        Debug.Log("X: " + axisX + " Y: " + axisY);
+    }
+}
