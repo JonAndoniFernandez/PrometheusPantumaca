0a744da698ef4a6f79d84748f25e14058d7f1491
mariot11 <32970004+mariot11@users.noreply.github.com>
2020-02-01 12:44:14 +0100

    parpadeo
    parpadeo

---
+++ PrometheusPantumaca/Assets/Resources/Scripts/EyesBlinkController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EyesBlinkController : MonoBehaviour
+{
+    //def variables
+    public GameObject EyesPrefab;
+    public GameObject[] enemies;
+    public string EnemyTag = "Enemy";
+    public float blinkTime = 1.0f;
+    private float time = 0.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        enemies = GameObject.FindGameObjectsWithTag(EnemyTag);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if el jugador ha cogido la llama
+        time += Time.deltaTime;
+
+        if(time >= blinkTime)
+        {
+            time = 0.0f;
+            for (int i = 0; i < enemies.Length; i++)
+            {
+                Instantiate(EyesPrefab, enemies[i].transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
