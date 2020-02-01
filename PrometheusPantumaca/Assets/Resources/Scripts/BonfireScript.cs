c6f1b95ffe5bb3f68cb3859c261802c840d610c6
JonanFdez <al315182@uji.es>
2020-02-01 15:14:19 +0100

    Movement and grabbing bonfire

---
+++ PrometheusPantumaca/Assets/Resources/Scripts/BonfireScript.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BonfireScript : MonoBehaviour
+{
+
+    public float clampSpeed = 1.5f;
+    public Transform playerDetected;
+    public Vector3 targetScale;
+    public bool grabbed = false;
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.tag == "Player" && Input.GetButton("Fire1") && !grabbed)
+        {
+            StartCoroutine( GrabbingBonfire(collision.transform.GetChild(0)));
+        }
+    }
+
+    IEnumerator GrabbingBonfire(Transform playerTarget)
+    {
+        //Vector2 target = new Vector2(playerTarget.position.x, playerTarget.position.y + 1);
+        targetScale = new Vector3(0.75f, 0.75f, 1);
+
+        while (Vector2.Distance(transform.position, playerTarget.position) > 0.2)
+        {
+            //target = new Vector2(playerTarget.position.x, playerTarget.position.y + 1);
+
+            transform.position = Vector2.Lerp(transform.position, playerTarget.position, Time.deltaTime * clampSpeed);
+            transform.localScale = Vector3.Lerp(transform.localScale, targetScale, Time.deltaTime * clampSpeed);
+            yield return null;
+        }
+
+        transform.position = playerTarget.position;
+        transform.localScale = targetScale;
+        playerTarget.GetComponentInParent<PlayerController>().grabbingBonfire = true;
+        playerDetected = playerTarget;
+        grabbed = true;
+    }
+
+
+    private void Update()
+    {
+        if(playerDetected != null)
+            transform.position = Vector2.Lerp(transform.position, playerDetected.position, Time.deltaTime * clampSpeed * 2);
+    }
+}
